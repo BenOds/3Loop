4d4ba5093d93cfd0ab7b6699ea0bc6240aed8fd3
benods <benods1138@gmail.com>
2022-08-22 19:05:13 +0200

    actualización

--- Assets/Scripts/Prefab/Chassing.cs
+++ Assets/Scripts/Prefab/Chassing.cs
@@ -5,29 +5,45 @@ using UnityEngine;
 public class Chassing : MonoBehaviour
 {
     GameObject player;
-    Vector3 positionPlayer;
+    public Vector3 positionPlayer;
 
     Vector3 direction;
     public float speed;
+
+    bool chassingBoolean;
+
     void Start()
     {
+        chassingBoolean = false;
         player = GameObject.FindGameObjectWithTag("Player"); 
-        
-       
+        Invoke("ChassingBoolean", 5);
     }
 
     void Update()
     {
-        positionPlayer = player.transform.position;
-        direction = positionPlayer - transform.position;
+        PositionPlayer();
         //transform.LookAt(player.transform);
-        Invoke("ChassingPlayer", 5);
+        ChassingPlayer();
     }
 
     void ChassingPlayer()
-    {
+    {   
+        if( chassingBoolean == true)
+        {
         //transform.Translate(direction * speed * Time.deltaTime);
         transform.Translate(Vector3.MoveTowards(transform.position, positionPlayer, 
                             speed * Time.deltaTime));
+        }
+    }
+
+    void ChassingBoolean()
+    {
+        chassingBoolean = true;
+    }
+
+    void PositionPlayer()
+    {
+        positionPlayer = player.transform.position;
+        direction = positionPlayer - transform.position;
     }
 }
